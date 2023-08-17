f489368e7cee54c8094696cd77dfbc76662c8ce6
Rashmi7205 <rashmiranjanbehera8260@gmail.com>
2023-08-17 19:18:59 +0530

    Adding app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Button from "./asssignment1/Button";
+import Header from "./asssignment1/Header";
+import Person from "./asssignment1/Person"
+
+function App() {
+
+  //For the button Function
+  function clicked(){
+    console.log("Button Clicked");
+  }
+
+  return (
+    <>
+      {/* Assignment 1 solutions */}
+  
+      {/* <Header title="Header"/>    
+      <Person name="john" age="21"/>
+      <Button text="Click Here" onClick={clicked}/> */}
+  
+
+
+    </>
+  )
+}
+
+export default App
